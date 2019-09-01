710bfccaea12c6619afbb84e7b31b594e235bec4
Walt Panfil <panfilwk@rose-hulman.edu>
2019-09-01 14:40:04 -0400

    Added support for loading secrets from shall file

--- TranslationApp.py
+++ TranslationApp.py
@@ -1,14 +1,19 @@
 """Full Translation App"""
 
+from os import getenv
 from pyrogram import Client, Filters
 from translate import Translator
 translator= Translator(to_lang="en", from_lang='zh')
 postScript= "\n\n\n TranslatorBot via pyrogram"
 
-#RecieveCh = "me"
-RecieveCh = -1001173826177 # For Real HK Channel
-SendCh = -324271754
-app = Client("my_account")
+RecieveCh = "me"
+# RecieveCh = -1001173826177 # For Real HK Channel
+SendCh = "me"
+# SendCh = -324271754
+
+api_id = getenv('API_ID')
+api_id_hash = getenv('API_ID_HASH')
+app = Client("my_account", api_id, api_id_hash)
 
 
 @app.on_message(Filters.chat(RecieveCh) & Filters.photo & Filters.caption)
@@ -28,7 +33,7 @@ def echoT(client, message):
     app.send_message(SendCh, translation)
     return
 
-    
+
 
 
 app.run()  # Automatically start() and idle()
