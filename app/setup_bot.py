60c6ddbdafa2be0531671b40439f00ea3b4890a3
Zachary Schafer <schafezp@gmail.com>
2019-09-09 20:38:15 -0400

    extract main logic into bot file

---
+++ app/setup_bot.py
@@ -0,0 +1,16 @@
+"""
+This is an entry point to setting configuring the application.
+"""
+
+from bot import run
+
+def setup():
+    print("Welcome to the TelegramTranslation app.")
+
+    print("Skip setup and run app directly")
+    run()
+
+
+
+if __name__ == "__main__":
+    setup()
